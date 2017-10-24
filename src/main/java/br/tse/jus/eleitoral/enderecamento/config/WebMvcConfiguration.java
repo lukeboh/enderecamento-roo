5615bbd03fb519990f6573becdabbf3567bd726e
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-10-24 10:48:52 -0200

    Comando roo: web mvc setup

---
+++ src/main/java/br/tse/jus/eleitoral/enderecamento/config/WebMvcConfiguration.java
@@ -0,0 +1,12 @@
+package br.tse.jus.eleitoral.enderecamento.config;
+import org.springframework.roo.addon.web.mvc.controller.annotations.config.RooWebMvcConfiguration;
+
+/**
+ * = WebMvcConfiguration
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooWebMvcConfiguration
+public class WebMvcConfiguration {
+}
