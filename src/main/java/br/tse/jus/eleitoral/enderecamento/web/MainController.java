e908f343c0aa004155d82768d140ddac081bfdb1
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-10-24 10:50:21 -0200

    Comando roo: web mvc view setup --type THYMELEAF

---
+++ src/main/java/br/tse/jus/eleitoral/enderecamento/web/MainController.java
@@ -0,0 +1,12 @@
+package br.tse.jus.eleitoral.enderecamento.web;
+import org.springframework.roo.addon.web.mvc.thymeleaf.annotations.RooThymeleafMainController;
+
+/**
+ * = MainController
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooThymeleafMainController
+public class MainController {
+}
