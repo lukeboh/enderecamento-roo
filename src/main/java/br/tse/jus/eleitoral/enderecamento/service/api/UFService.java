3b33c718900ec674f80c3e9bafbd42ade5cb5aee
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-10-24 10:47:36 -0200

    Comando roo: service --all

---
+++ src/main/java/br/tse/jus/eleitoral/enderecamento/service/api/UFService.java
@@ -0,0 +1,13 @@
+package br.tse.jus.eleitoral.enderecamento.service.api;
+import br.tse.jus.eleitoral.enderecamento.dominio.UF;
+import org.springframework.roo.addon.layers.service.annotations.RooService;
+
+/**
+ * = UFService
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooService(entity = UF.class)
+public interface UFService {
+}
---
+++ src/main/java/br/tse/jus/eleitoral/enderecamento/service/impl/UFServiceImpl.java
@@ -0,0 +1,13 @@
+package br.tse.jus.eleitoral.enderecamento.service.impl;
+import br.tse.jus.eleitoral.enderecamento.service.api.UFService;
+import org.springframework.roo.addon.layers.service.annotations.RooServiceImpl;
+
+/**
+ * = UFServiceImpl
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooServiceImpl(service = UFService.class)
+public class UFServiceImpl implements UFService {
+}
