fe4c6d8ca51ddd945c621dc91ec9dbf95dd4988b
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-10-24 10:46:12 -0200

    Criando UF com dois campos

---
+++ src/main/java/br/tse/jus/eleitoral/enderecamento/dominio/UF.java
@@ -0,0 +1,52 @@
+package br.tse.jus.eleitoral.enderecamento.dominio;
+import org.springframework.roo.addon.javabean.annotations.RooEquals;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+import org.springframework.roo.addon.javabean.annotations.RooToString;
+import org.springframework.roo.addon.jpa.annotations.entity.RooJpaEntity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+
+/**
+ * = UF
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooJavaBean
+@RooToString
+@RooJpaEntity
+@RooEquals(isJpaEntity = true)
+public class UF {
+
+    /**
+     * TODO Auto-generated attribute documentation
+     *
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /**
+     * TODO Auto-generated attribute documentation
+     *
+     */
+    @Version
+    private Integer version;
+
+    /**
+     * TODO Auto-generated attribute documentation
+     *
+     */
+    @NotNull
+    private String sigla;
+
+    /**
+     * TODO Auto-generated attribute documentation
+     *
+     */
+    @NotNull
+    private String nome;
+}
