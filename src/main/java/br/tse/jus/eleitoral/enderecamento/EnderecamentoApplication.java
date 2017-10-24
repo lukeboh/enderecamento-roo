f3ba78237345fff97f18ae91b7b031c93b4c9af2
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-10-24 10:42:47 -0200

    primeiro commit

---
+++ src/main/java/br/tse/jus/eleitoral/enderecamento/EnderecamentoApplication.java
@@ -0,0 +1,23 @@
+package br.tse.jus.eleitoral.enderecamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = EnderecamentoApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class EnderecamentoApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EnderecamentoApplication.class, args);
+    }
+}
